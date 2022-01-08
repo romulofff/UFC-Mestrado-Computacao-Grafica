602ed47f987b9dcdbd5724bfa34faec129f0d8cd
romulofff <romulofffufc@gmail.com>
2022-01-08 15:19:42 -0300

    PyOpenGL tutorial script

---
+++ PyOpenGL_Example/main.py
@@ -0,0 +1,42 @@
+import OpenGL
+from OpenGL.GL import *
+from OpenGL.GLUT import *
+from OpenGL.GLU import *
+
+w,h= 500,500
+def square():
+    glBegin(GL_QUADS)
+    glVertex2f(100, 100)
+    glVertex2f(200, 100)
+    glVertex2f(200, 200)
+    glVertex2f(100, 200)
+    glEnd()
+
+def iterate():
+    glViewport(0, 0, 500, 500)
+    glMatrixMode(GL_PROJECTION)
+    glLoadIdentity()
+    glOrtho(0.0, 500, 0.0, 500, 0.0, 1.0)
+    glMatrixMode (GL_MODELVIEW)
+    glLoadIdentity()
+
+def showScreen():
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+    glLoadIdentity()
+    iterate()
+    glColor3f(1.0, 0.0, 3.0)
+    square()
+    glutSwapBuffers()
+    
+# Initialize a glut instance which will allow us to customize our window
+glutInit()
+glutInitDisplayMode(GLUT_RGBA)  # Set the display mode to be colored
+glutInitWindowSize(w, h)   # Set the width and height of your window
+# Set the position at which this windows should appear
+glutInitWindowPosition(0, 0)
+wind = glutCreateWindow("OpenGL Coding Practice")  # Give your window a title
+# Tell OpenGL to call the showScreen method continuously
+glutDisplayFunc(showScreen)
+# Draw any graphics or shapes in the showScreen function at all times
+glutIdleFunc(showScreen)
+glutMainLoop()  # Keeps the window created above displaying/running in a loop
