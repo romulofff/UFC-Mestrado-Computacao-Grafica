b9d0eef7c5a78640be13f4e9412af4f9aede7141
Alexandre <magnomont12@gmail.com>
2022-01-11 22:31:30 -0300

    Camera definida com a matriz. Produto escalar, normalizaçao e alguns calculos basicos feitos

--- PyOpenGL_Example/main.py
+++ PyOpenGL_Example/main.py
@@ -3,9 +3,71 @@ from OpenGL.GL import *
 from OpenGL.GLUT import *
 from OpenGL.GLU import *
 from random import randrange, uniform
+import math
+import numpy as np
+
+
+def rotate_vector(vector, angle, axis):
+    """
+    Vetor de entrada,o tamanho da rotação e o eixo que deve ser girado.
+    """
+    cos_positive = math.degrees(math.cos(angle))
+    sen_positive = math.degrees(math.sen(angle))
+    sen_negative = -math.degrees(math.sen(angle))
+    if axis == 'z':
+        matrix_z = np.matrix = [[cos_positive, sen_negative, 0],
+                                [sen_positive, cos_positive, 0],
+                                [0,            0,            1],
+                                ]
+
+def cross_product(point_a, point_b):
+    x = point_a[0,1]*point_b[0,2] - point_a[0,2] *point_b[0,1]
+    y = point_a[0,2]*point_b[0,0] - point_a[0,0] *point_b[0,2]
+    z = point_a[0,0]*point_b[0,1] - point_a[0,1] *point_b[0,0]
+    return np.matrix([x,y,z])
+
+def normalize_vector(vector):
+    norm = np.sqrt(np.square(vector).sum())
+    return vector / norm
+
+def dot(vector1, vector2):
+    return vector1[0,0] * vector2[0,0] + vector1[0,1] * vector2[0,1] + vector1[0,2] * vector2[0,2]
+    
 
 w,h= 800,600
 matrix = [ [ [uniform(0,1),uniform(0,1),uniform(0,1)] for a in range(h)] for b in range(w)]
+
+class camera:
+    def __init__(self, point_xyz, lookat,view_up, sagital_point_xyz):
+        self.point_xyz = point_xyz
+        self.sagital_point_xyz = sagital_point_xyz
+        self.vector_k = self.origin_cordinates_k(point_xyz, lookat)
+        self.vector_i = self.origin_cordinates_i(point_xyz, lookat, view_up)
+        self.vector_j = normalize_vector(cross_product(self.vector_k,self.vector_i))
+        self.world_to_camera = np.matrix([[ self.vector_i[0,0], self.vector_i[0,1], self.vector_i[0,2], -(dot(self.vector_i,self.point_xyz)) ],
+                                         [ self.vector_j[0,0], self.vector_j[0,1], self.vector_j[0,2], -(dot(self.vector_j,self.point_xyz)) ],
+                                         [ self.vector_k[0,0], self.vector_k[0,1], self.vector_k[0,2], -(dot(self.vector_k,self.point_xyz)) ],
+                                         [ 0,0,0,1]]
+                                         )
+    
+    def origin_cordinates_k(self,point_xyz, lookat):
+        vector_k = point_xyz - lookat
+        vector_k = normalize_vector(vector_k)
+        return vector_k
+
+    def origin_cordinates_i(self,point_xyz, lookat, view_up):
+        v_up = view_up - point_xyz
+        vector_i = cross_product(v_up, self.vector_k)
+        vector_i = normalize_vector(vector_i)
+        return vector_i
+    
+
+
+
+
+view = camera(point_xyz=np.matrix([10,10,10]), lookat=np.matrix([2,0,0]), view_up=np.matrix([0,2,0]), sagital_point_xyz=np.matrix([2,2,2]))
+print(view.world_to_camera)
+
 
 def init_matrix():
     for a in range(w):
