6efc2b20d9e582a7aebbd4158b4a9e1eb6f2f28c
Alexandre <magnomont12@gmail.com>
2022-01-15 14:15:15 -0300

    Add class Sphere

---
+++ PyOpenGL_Example/sphere.py
@@ -0,0 +1,24 @@
+from point import Point
+from camera import Camera
+from utils import *
+
+
+class Sphere:
+    def __init__(self,center, radius) -> None:
+        self.center = center
+        self.radius = radius
+        self.center_camera = None
+
+    def get_center_camera(self,camera):
+        self.center_camera = point_matrix_mult(self.center, camera.world_to_camera)
+
+
+point_xyz = Point(0,0,0)
+lookat = Point(0, 0, -1)
+view_up = Point(0, 1, 0)
+view = Camera(point_xyz=point_xyz,
+                lookat=lookat, view_up=view_up)
+
+sphere = Sphere(Point(30,30,30), 30)
+sphere.get_center_camera(view)
+print(sphere.center_camera)
