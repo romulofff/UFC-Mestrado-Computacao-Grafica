a5df9c3f9659d6afe568df5d7abaa8d276f375c9
romulofff <romulofffufc@gmail.com>
2022-01-17 23:10:08 -0300

    Code formatting

--- PyOpenGL_Example/collider.py
+++ PyOpenGL_Example/collider.py
@@ -1,7 +1,8 @@
-from sphere import Sphere
 from cilinder import Cilinder
 from cube import Cube
+from sphere import Sphere
 from utils import *
+
 
 class Collider:
     def __init__(self) -> None:
@@ -17,7 +18,8 @@ class Collider:
                 return self._collision_ray_cube(ray, object)
 
     def _collision_ray_sphere(self, ray, sphere):
-        v = Point.from_matrix(ray.frist_point.matrix - sphere.center_camera.matrix)
+        v = Point.from_matrix(ray.frist_point.matrix -
+                              sphere.center_camera.matrix)
         a = ray.measure**2 * dot(ray.direction, ray.direction)
         b = ray.measure * dot(ray.direction, v)
         c = dot(v, v) - sphere.radius**2
