0da23d0a6d84de9542d69268434ed3545fe17b6f
romulofff <romulofffufc@gmail.com>
2022-02-01 17:50:44 -0300

    Add cone collision

--- PyOpenGL_Example/cone.py
+++ PyOpenGL_Example/cone.py
@@ -1,24 +1,30 @@
 from camera import Camera
+from plane import Plane
 from point import Point
 from utils import *
 
 
 class Cone:
-    def __init__(self, P, vertex, n, theta, height, radius, center=None) -> None:
-        self.P = P
+    def __init__(self, center, radius, height, n, vertex=None) -> None:
         self.vertex = vertex
-        self.n = n
-        self.theta = theta
+        self.n = normalize_vector(n)
         self.height = height
         self.radius = radius
-        if center:
-            self.vertex = center + height*n
-        if 0 > dot((self.vertex - self.P), n) > self.height:
-            raise Exception("Invalid Cone data.")
+        self.center = center
+        self.base = Plane(self.center, self.n)
 
     def get_center_camera(self, camera):
         self.center_camera = Point.from_matrix(
             point_matrix_mult(self.center, camera.world_to_camera))
+
+    def valid_point_cone(self, vertex: Point, p_int: Point):
+        v_aux = Point.from_matrix(vertex.matrix - p_int.matrix)
+        escalar_aux = dot(v_aux, self.n)
+        if escalar_aux < 0.00000000001 and escalar_aux > 0.00000000001:
+            escalar_aux = 0
+        if escalar_aux >= 0 and escalar_aux <= self.height:
+            return True
+        else: return False
 
 
 if __name__ == '__main__':
